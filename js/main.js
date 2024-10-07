785d4b2f889ddd0c9a92e696a548fd06258788df
robbyae <robbyastrong@gmail.com>
2024-10-07 10:04:27 -0500

    more

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-  $('#hand-svg').click(function(){
-    $('#icxc-window').css('display','block');
-  });
+$('#mp-oy').click(function(){
+  $('#title').html('ΜΡ ΘΥ')
+  $('#writing').html(
+    'An abbreviation of the Greek "Μήτηρ τοῦ Θεοῦ", meaning "Mother of God".');
 });
+$('#hagia-sophia').click(function(){
+  $('#title').html('Hagia Sophia')
+  $('#writing').html(
+    'A model of the Hagia Sophia being presented to the Virgin Mary.' + '<br><br>' +
+  '<img class="side-img" src="/assets/img/hagia-sophia.jpg"></img>');
+});
+
+// $(function(){
+//   $("#zoom").anythingZoomer();
+//   });
