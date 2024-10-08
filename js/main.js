b1a7e73a32f382551f6be4444cdca39bee4b62c9
robbyae <150072088+robbyae@users.noreply.github.com>
2024-10-08 14:44:28 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('#hagia-sophia').click(function(){
   $('#title').html('Hagia Sophia')
   $('#writing').html(
     'A model of the Hagia Sophia being presented to the Virgin Mary.' + '<br><br>' +
-  '<img class="side-img" src="../assets/img/hagia-sophia.jpg"></img>' + '<br><br>' +
+  '<img class="side-img" src="./assets/img/hagia-sophia.jpg"></img>' + '<br><br>' +
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 });
 $('#theotokos').click(function(){
