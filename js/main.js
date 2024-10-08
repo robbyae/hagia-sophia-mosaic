ea89aef5d3e8716c55004f2a95d8a10388133ec7
robbyae <150072088+robbyae@users.noreply.github.com>
2024-10-08 14:01:15 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,23 +1,56 @@
 $('#mp-oy').click(function(){
   $('#title').html('ΜΡ ΘΥ')
   $('#writing').html(
-    'An abbreviation of the Greek "Μήτηρ τοῦ Θεοῦ", meaning "Mother of God". KEEP GOING NEVER STOP WERE ALL GONNA MAKE IT.');
+    'An abbreviation of the Greek "Μήτηρ τοῦ Θεοῦ", meaning "Mother of God". Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 });
 $('#hagia-sophia').click(function(){
   $('#title').html('Hagia Sophia')
   $('#writing').html(
     'A model of the Hagia Sophia being presented to the Virgin Mary.' + '<br><br>' +
-  '<img class="side-img" src="/assets/img/hagia-sophia.jpg"></img>');
+  '<img class="side-img" src="/assets/img/hagia-sophia.jpg"></img>' + '<br><br>' +
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 });
 $('#theotokos').click(function(){
   $('#title').html('Theotokos')
   $('#writing').html(
-    'The Virgin Mother Mary holding the Child Christ. The Greek title <i>Theotokos</i>, meaning God-Bearer, is used to describe her.');
+    'The Virgin Mother Mary holding the Child Christ. The Greek title <i>Theotokos</i>, meaning God-Bearer, is used to describe her. Here she wears a maphorion. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 });
-
-// $(function(){
-//   $("#zoom").anythingZoomer();
-//   });
+$('#jesus').click(function(){
+  $('#title').html("The Child Christ")
+  $('#writing').html(
+    'Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas ut ipsum justo dictum nullam enim penatibus sapien. Vestibulum posuere sem convallis hendrerit eros efficitur torquent. Convallis proin nullam class molestie maximus. Torquent dolor ut hac volutpat curae eu. Tempus molestie habitasse id adipiscing senectus est metus ac tincidunt. Cras massa libero est litora velit metus sagittis massa. Tincidunt tortor turpis aliquet elementum sit, ornare nisl quisque. Himenaeos purus integer; mattis rutrum ut netus ligula. Senectus eleifend porta vitae metus sociosqu duis eget natoque. Vehicula malesuada magna sapien scelerisque eget fames. Velit malesuada ex nostra bibendum diam turpis ligula maximus? Ornare egestas pellentesque laoreet torquent netus luctus tempor. Vestibulum semper ut rhoncus adipiscing molestie interdum. Ullamcorper mi fermentum ac placerat platea placerat commodo donec. Morbi curae dictum fermentum sapien ullamcorper dictum auctor lorem. Aenean ut primis leo tincidunt donec porttitor nostra tortor eleifend. Per adipiscing viverra erat porta senectus ad. Tristique aenean risus dui dui fermentum suspendisse scelerisque accumsan. Praesent curabitur orci viverra lobortis lacus scelerisque pharetra. Mauris faucibus est scelerisque magna potenti convallis quis convallis dictum. Imperdiet ad feugiat ut sed dolor pellentesque. Conubia quisque congue rutrum aliquam posuere velit class leo. Fusce eleifend etiam lobortis nulla per. Himenaeos mus donec duis arcu at aliquet.'
+  )
+});
+$('#justinian-i').click(function(){
+  $('#title').html("Justinian I")
+  $('#writing').html(
+    'Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas ut ipsum justo dictum nullam enim penatibus sapien. Vestibulum posuere sem convallis hendrerit eros efficitur torquent. Convallis proin nullam class molestie maximus. Torquent dolor ut hac volutpat curae eu. Tempus molestie habitasse id adipiscing senectus est metus ac tincidunt. Cras massa libero est litora velit metus sagittis massa. Tincidunt tortor turpis aliquet elementum sit, ornare nisl quisque. Himenaeos purus integer; mattis rutrum ut netus ligula. Senectus eleifend porta vitae metus sociosqu duis eget natoque. Vehicula malesuada magna sapien scelerisque eget fames. Velit malesuada ex nostra bibendum diam turpis ligula maximus? Ornare egestas pellentesque laoreet torquent netus luctus tempor. Vestibulum semper ut rhoncus adipiscing molestie interdum. Ullamcorper mi fermentum ac placerat platea placerat commodo donec. Morbi curae dictum fermentum sapien ullamcorper dictum auctor lorem. Aenean ut primis leo tincidunt donec porttitor nostra tortor eleifend. Per adipiscing viverra erat porta senectus ad. Tristique aenean risus dui dui fermentum suspendisse scelerisque accumsan. Praesent curabitur orci viverra lobortis lacus scelerisque pharetra. Mauris faucibus est scelerisque magna potenti convallis quis convallis dictum. Imperdiet ad feugiat ut sed dolor pellentesque. Conubia quisque congue rutrum aliquam posuere velit class leo. Fusce eleifend etiam lobortis nulla per. Himenaeos mus donec duis arcu at aliquet.'
+  )
+});
+$('#constantine').click(function(){
+  $('#title').html("Constantine I")
+  $('#writing').html(
+    'Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas ut ipsum justo dictum nullam enim penatibus sapien. Vestibulum posuere sem convallis hendrerit eros efficitur torquent. Convallis proin nullam class molestie maximus. Torquent dolor ut hac volutpat curae eu. Tempus molestie habitasse id adipiscing senectus est metus ac tincidunt. Cras massa libero est litora velit metus sagittis massa. Tincidunt tortor turpis aliquet elementum sit, ornare nisl quisque. Himenaeos purus integer; mattis rutrum ut netus ligula. Senectus eleifend porta vitae metus sociosqu duis eget natoque. Vehicula malesuada magna sapien scelerisque eget fames. Velit malesuada ex nostra bibendum diam turpis ligula maximus? Ornare egestas pellentesque laoreet torquent netus luctus tempor. Vestibulum semper ut rhoncus adipiscing molestie interdum. Ullamcorper mi fermentum ac placerat platea placerat commodo donec. Morbi curae dictum fermentum sapien ullamcorper dictum auctor lorem. Aenean ut primis leo tincidunt donec porttitor nostra tortor eleifend. Per adipiscing viverra erat porta senectus ad. Tristique aenean risus dui dui fermentum suspendisse scelerisque accumsan. Praesent curabitur orci viverra lobortis lacus scelerisque pharetra. Mauris faucibus est scelerisque magna potenti convallis quis convallis dictum. Imperdiet ad feugiat ut sed dolor pellentesque. Conubia quisque congue rutrum aliquam posuere velit class leo. Fusce eleifend etiam lobortis nulla per. Himenaeos mus donec duis arcu at aliquet.'
+  )
+});
+$('#constantinople').click(function(){
+  $('#title').html("Constantinople")
+  $('#writing').html(
+    'Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas ut ipsum justo dictum nullam enim penatibus sapien. Vestibulum posuere sem convallis hendrerit eros efficitur torquent. Convallis proin nullam class molestie maximus. Torquent dolor ut hac volutpat curae eu. Tempus molestie habitasse id adipiscing senectus est metus ac tincidunt. Cras massa libero est litora velit metus sagittis massa. Tincidunt tortor turpis aliquet elementum sit, ornare nisl quisque. Himenaeos purus integer; mattis rutrum ut netus ligula. Senectus eleifend porta vitae metus sociosqu duis eget natoque. Vehicula malesuada magna sapien scelerisque eget fames. Velit malesuada ex nostra bibendum diam turpis ligula maximus? Ornare egestas pellentesque laoreet torquent netus luctus tempor. Vestibulum semper ut rhoncus adipiscing molestie interdum. Ullamcorper mi fermentum ac placerat platea placerat commodo donec. Morbi curae dictum fermentum sapien ullamcorper dictum auctor lorem. Aenean ut primis leo tincidunt donec porttitor nostra tortor eleifend. Per adipiscing viverra erat porta senectus ad. Tristique aenean risus dui dui fermentum suspendisse scelerisque accumsan. Praesent curabitur orci viverra lobortis lacus scelerisque pharetra. Mauris faucibus est scelerisque magna potenti convallis quis convallis dictum. Imperdiet ad feugiat ut sed dolor pellentesque. Conubia quisque congue rutrum aliquam posuere velit class leo. Fusce eleifend etiam lobortis nulla per. Himenaeos mus donec duis arcu at aliquet.'
+  )
+});
+$('#left-text').click(function(){
+  $('#title').html("Greek Text")
+  $('#writing').html(
+    'Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas ut ipsum justo dictum nullam enim penatibus sapien. Vestibulum posuere sem convallis hendrerit eros efficitur torquent. Convallis proin nullam class molestie maximus. Torquent dolor ut hac volutpat curae eu. Tempus molestie habitasse id adipiscing senectus est metus ac tincidunt. Cras massa libero est litora velit metus sagittis massa. Tincidunt tortor turpis aliquet elementum sit, ornare nisl quisque. Himenaeos purus integer; mattis rutrum ut netus ligula. Senectus eleifend porta vitae metus sociosqu duis eget natoque. Vehicula malesuada magna sapien scelerisque eget fames. Velit malesuada ex nostra bibendum diam turpis ligula maximus? Ornare egestas pellentesque laoreet torquent netus luctus tempor. Vestibulum semper ut rhoncus adipiscing molestie interdum. Ullamcorper mi fermentum ac placerat platea placerat commodo donec. Morbi curae dictum fermentum sapien ullamcorper dictum auctor lorem. Aenean ut primis leo tincidunt donec porttitor nostra tortor eleifend. Per adipiscing viverra erat porta senectus ad. Tristique aenean risus dui dui fermentum suspendisse scelerisque accumsan. Praesent curabitur orci viverra lobortis lacus scelerisque pharetra. Mauris faucibus est scelerisque magna potenti convallis quis convallis dictum. Imperdiet ad feugiat ut sed dolor pellentesque. Conubia quisque congue rutrum aliquam posuere velit class leo. Fusce eleifend etiam lobortis nulla per. Himenaeos mus donec duis arcu at aliquet.'
+  )
+});
+$('#right-text').click(function(){
+  $('#title').html("Greek Text")
+  $('#writing').html(
+    'Lorem ipsum odor amet, consectetuer adipiscing elit. Maecenas ut ipsum justo dictum nullam enim penatibus sapien. Vestibulum posuere sem convallis hendrerit eros efficitur torquent. Convallis proin nullam class molestie maximus. Torquent dolor ut hac volutpat curae eu. Tempus molestie habitasse id adipiscing senectus est metus ac tincidunt. Cras massa libero est litora velit metus sagittis massa. Tincidunt tortor turpis aliquet elementum sit, ornare nisl quisque. Himenaeos purus integer; mattis rutrum ut netus ligula. Senectus eleifend porta vitae metus sociosqu duis eget natoque. Vehicula malesuada magna sapien scelerisque eget fames. Velit malesuada ex nostra bibendum diam turpis ligula maximus? Ornare egestas pellentesque laoreet torquent netus luctus tempor. Vestibulum semper ut rhoncus adipiscing molestie interdum. Ullamcorper mi fermentum ac placerat platea placerat commodo donec. Morbi curae dictum fermentum sapien ullamcorper dictum auctor lorem. Aenean ut primis leo tincidunt donec porttitor nostra tortor eleifend. Per adipiscing viverra erat porta senectus ad. Tristique aenean risus dui dui fermentum suspendisse scelerisque accumsan. Praesent curabitur orci viverra lobortis lacus scelerisque pharetra. Mauris faucibus est scelerisque magna potenti convallis quis convallis dictum. Imperdiet ad feugiat ut sed dolor pellentesque. Conubia quisque congue rutrum aliquam posuere velit class leo. Fusce eleifend etiam lobortis nulla per. Himenaeos mus donec duis arcu at aliquet.'
+  )
+});
 
 // SIZE RIGHT TEXT BOX //
 $(document).ready(function(){
@@ -28,4 +61,16 @@ $(document).ready(function(){
 });
 
 // ZOOM //
+$('#zoom').click(function(){
+  if($('#main-svg').css("display","block")) {
+    $('#main-svg').css("display","none");
+    $('#zoom-box').zoom({ on:'mouseover' });
+  }
+  else if($('#main-svg').css("display","none")){
+    $('#main-svg').css("display","block");
+  };
+});
 
+$(document).ready(function(){
+  
+});
